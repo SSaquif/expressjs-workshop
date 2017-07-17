be110627e9429a45f748b368f919ed2d359c679b
Sadnan Saquif <ssaquif@gmail.com>
2017-07-17 21:18:27 +0000

    Answer 4

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var express = require('express'); //importing express library 
 var app = express();  //We are using a expressJS web server
+var request = require('request-promise');
+var mysql = require('promise-mysql');
+var RedditAPI = require('../../redditCommandLine/reddit-nodejs-api/reddit');
+
+//database connection
+var dbConn = mysql.createPool({
+  host     : 'localhost',
+  user     : 'ssaquif',
+  password : '',
+  database: 'reddit',
+  connectionLimit: 10
+});
+
+//new Reddit API object
+var redditFunctions = new RedditAPI(dbConn);
 
 //Endpoint for index page
 app.get('/', function (request, respond) {
@@ -46,8 +61,8 @@ app.get('/calculator/:operation', function(request, respond)
   var result = {};
   if(request.params.operation === ':add')
   {
-    //this splits on the dilimiter ':'' and puts the split string in an array
-    //:add = [:,add]
+    //NTS:this splits on the dilimiter ':'' and puts the split string in an array
+    //so, :add = ['','add']
     result.operation = request.params.operation.split(':')[1];
     //console.log(request.operation);
     result.firstOperand = parseInt(request.query.num1);
@@ -68,6 +83,36 @@ app.get('/calculator/:operation', function(request, respond)
     respond.send("Invalid operation selected");
   }
 });
+
+//Endpoint for getting reddit posts
+app.get('/posts', function(request, respond)
+{
+  redditFunctions.getAllPosts()
+  .then(results => {
+      var postList = "";
+      //first make cancate all posts into a string
+      results.forEach(post => {
+        postList = postList + 
+                  ` <li class="post-item">
+                      <h2 class="post-item__title">
+                        <a href="${post.url}">${post.title}</a>
+                      </h2>
+                        <p>Created by ${post.userData.userName}</p>
+                    </li> `
+      })
+      //second send them
+      respond.send(`<!DOCTYPE html>
+                    <body>
+                      <div id="posts">
+                        <h1>List of posts</h1>
+                        <ul class="posts-list">`
+                        + postList +
+                        `</ul>
+                      </div>
+                    </body>`);
+  });
+});
+
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
