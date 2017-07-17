389794b5efc37f6df4182a8cfa439495534d5f5f
Sadnan Saquif <ssaquif@gmail.com>
2017-07-17 19:32:12 +0000

    Answers 1-3

--- index.js
+++ index.js
@@ -1,12 +1,73 @@
-var express = require('express');
-var app = express();
+var express = require('express'); //importing express library 
+var app = express();  //We are using a expressJS web server
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//Endpoint for index page
+app.get('/', function (request, respond) {
+  respond.send('Hello World!');
 });
 
+//Q1 and 2 Using Query String Params, (i.e, ?name=...)
+//Endpoint for /hello
+app.get('/hello', function (request,respond) 
+{
+  //console.log(request);
+  console.log(request.query);
+  if (request.query.hasOwnProperty("name")){
+    respond.send(`
+      <!DOCTYPE html>
+        <body>
+          <h1>Hello ` + request.query.name + `!<h1>
+        </body>
+    `);
+  }
+  else {
+    respond.send(`
+      <!DOCTYPE html>
+      <body>
+        <h1>Hello World!<h1>
+      </body>
+    `);
+  }
+});
 
-
+//Q3 Using Request Params, (i.e :operation)
+//Endpoint for calculator
+app.get('/calculator/:operation', function(request, respond)
+{
+  console.log(request.params.operation);
+  if(!request.query.hasOwnProperty("num1"))
+  {
+    request.query.num1 = 0;
+  }
+  if(!request.query.hasOwnProperty("num2"))
+  {
+    request.query.num2 = 0;
+  }
+  var result = {};
+  if(request.params.operation === ':add')
+  {
+    //this splits on the dilimiter ':'' and puts the split string in an array
+    //:add = [:,add]
+    result.operation = request.params.operation.split(':')[1];
+    //console.log(request.operation);
+    result.firstOperand = parseInt(request.query.num1);
+    result.secondOperand = parseInt(request.query.num2);
+    result.solution = result.firstOperand + result.secondOperand;
+    respond.send(JSON.stringify(result));
+  }
+  else if (request.params.operation === ':multiply')
+  {
+    result.operation = request.params.operation.split(':')[1];
+    result.firstOperand = parseInt(request.query.num1);
+    result.secondOperand = parseInt(request.query.num2);
+    result.solution = result.firstOperand * result.secondOperand;
+    respond.send(JSON.stringify(result));
+  }
+  else 
+  {
+    respond.send("Invalid operation selected");
+  }
+});
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
